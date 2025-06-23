ee254b129a1798ab99f96e996fb10a6ad9a1a7c4
Rakibul Alam <rakibulalam59900@gmail.com>
2025-06-23 14:55:08 +0600

    app.js modified

--- back-end/app.js
+++ back-end/app.js
@@ -43,6 +43,10 @@ app.use(cors({
 app.use('/registration', resisterRouter);
 app.use('/login',loginRouter)
 app.use('/chats',chatRouter)
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
 app.use((req, res, next) => {
     res.status(404).json({ message: 'Route not found' });
   });
